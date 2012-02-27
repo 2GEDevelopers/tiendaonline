89799b21fc7a7ed768e9544c412a1f649417537c
Edward De Sousa <desousas@hotmail.com>
2012-02-27 07:43:27 -0430

    Avances: -Construido la maquetacion del layout principal -Agregado los modelos con sus respectivos formularios y filtros: 	-Usuario 	-Direccion 	-Estado 	-Ciudad 	-Apadrinado -Ignorado archivo de configuracion de la base de datos

---
+++ lib/form/doctrine/DireccionForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Direccion form.
+ *
+ * @package    tiendaonline
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class DireccionForm extends BaseDireccionForm
+{
+  public function configure()
+  {
+  }
+}
